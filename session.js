f4719e0c0d5dd776d5c35b7b4eef3b626d04de43
jonniejontez <jomollo086@gmail.com>
2025-04-08 23:25:42 +0300

    Jonny

--- session.js
+++ session.js
@@ -40,9 +40,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~GYlhyA4L#PIHcjbXBzCZBe6-wRk7SeLH-_nkT7D8zbb5Vjy3iCoY,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "jonniejontez",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "./session",
